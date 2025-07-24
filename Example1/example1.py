8f8562881d62d85c8e2bde5c26308f3c2d22331e
Stephane Hubart <hubart.stephane@gmail.com>
2025-07-24 23:05:35 +0200

    example1 now creates cicles on mouse click

--- Example1/example1.py
+++ Example1/example1.py
@@ -11,12 +11,46 @@ size = (800, 600)
 screen = pygame.display.set_mode(size, pygame.RESIZABLE)
 pygame.display.set_caption("Fenêtre rouge redimensionnable")
 
+
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def Print(self):
+        print("x =", self.x)
+        print("y =", self.y)
+        
+
+class Circle:
+
+    def __init__(self, x, y, radius):
+        self.position = Point(x, y)
+        self.radius = radius
+
+    def Print(self):
+        self.position.Print()
+        print("radius =", self.radius)
+
+    def Draw(self, screen):
+        pygame.draw.circle(screen, (0, 255, 0), (int(self.position.x), int(self.position.y)), self.radius)
+
+my_circles = []
+
 # Boucle principale
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+
+        elif event.type == pygame.MOUSEBUTTONUP:
+            c = Circle(event.pos[0], event.pos[1], 50)
+            my_circles.append(c);
+
+
         elif event.type == pygame.VIDEORESIZE:
             # Mettre à jour la taille de la fenêtre
             size = event.size
@@ -24,6 +58,10 @@ while running:
 
     # Remplissage de l'écran en rouge
     screen.fill((255, 0, 0))
+
+    for c in my_circles:
+        c.Draw(screen)
+
     pygame.display.flip()
 
 # Fermeture propre
